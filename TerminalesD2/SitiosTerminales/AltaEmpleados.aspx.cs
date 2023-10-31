27c95c540234795802a2dbe226b3f7e753823f12
Diego Fernandez <diegofernandez.dnfb@gmail.com>
2023-10-31 10:42:45 -0300

    actualizado

---
+++ TerminalesD2/SitiosTerminales/AltaEmpleados.aspx.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Drawing;
+using System.Web.UI.WebControls;
+using ModeloEF;
+
+public partial class AltaEmpleados : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LimpiarDatos();
+        }
+    }
+
+    private void LimpiarDatos()
+    {
+        txtUsu.Text = "";
+        txtNombre.Text = "";
+        txtPass.Text = "";
+
+        btnBuscar.Enabled = true;
+        btnLimpiar.Enabled = true;
+        btnCrear.Enabled = false;
+        btnEliminar.Enabled = false;
+        btnModificar.Enabled = false;
+    }
+
+    protected void btnLimpiar_Click(object sender, EventArgs e)
+    {
+        LimpiarDatos();
+    }
+
+    protected void btnCrear_Click(object sender, EventArgs e)
+    {
+        Obligatorio2Entities ObContexto = null;
+        Empleados unE = null;
+
+        try
+        {
+            ObContexto = (Obligatorio2Entities)Session["Contexto"];
+
+            unE = new Empleados()
+            {
+                Usuario = txtUsu.Text.Trim(),
+                NombreCompleto = txtNombre.Text.Trim(),
+                Pass = txtPass.Text.Trim()
+            };
+
+            Validar.ValidarEmpleados(unE);
+        }
+        catch (Exception ex)
+        {
+            lblError.ForeColor = Color.Red;
+            lblError.Text = ex.Message;
+            return;
+        }
+
+        try
+        {
+            ObContexto.Empleados.Add(unE); 
+            ObContexto.SaveChanges();
+
+            lblError.ForeColor = Color.Green;
+            lblError.Text = "Usuario ingresado correctamente.";
+
+            LimpiarDatos();
+
+        }
+        catch (Exception ex)
+        {
+            ObContexto.Entry(unE).State = System.Data.Entity.EntityState.Detached;
+            lblError.ForeColor = Color.Red;
+            lblError.Text = ex.Message;
+        }
+    }
+
+    protected void btnBuscar_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Obligatorio2Entities ObContexto = (Obligatorio2Entities)Session["Contexto"];
+
+            string usu = txtUsu.Text.Trim();
+            Empleados unE = ObContexto.Empleados.Where(x => x.Usuario == usu).FirstOrDefault();
+
+            if (unE == null)
+            {
+                txtPass.Text = "";
+                txtNombre.Text = "";
+
+                btnCrear.Enabled = true;
+                btnEliminar.Enabled = false;
+                btnModificar.Enabled = false;
+            }
+            else
+            {
+                txtNombre.Text = unE.NombreCompleto;
+                txtPass.Text = unE.Pass;
+
+                btnCrear.Enabled = false;
+                btnEliminar.Enabled = true;
+                btnModificar.Enabled = true;
+                Session["Empleados"] = unE;
+            }
+        }
+        catch (Exception ex)
+        {
+            lblError.ForeColor = Color.Red;
+            lblError.Text = ex.Message;
+        }
+    }
+
+    protected void btnModificar_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Obligatorio2Entities ObContexto = (Obligatorio2Entities)Session["Contexto"];
+
+            Empleados unE = (Empleados)Session["Empleados"];
+            Empleados empLogueado = (Empleados)Session["Usuario"];
+
+            if (unE.Usuario != empLogueado.Usuario)
+            {
+                lblError.ForeColor = Color.Blue;
+                lblError.Text = "No se puede cambiar la contraseña de otro usuario.";
+                return;
+            }
+
+            unE.Pass = txtPass.Text.Trim();
+            unE.NombreCompleto = txtNombre.Text.Trim();
+
+            Validar.ValidarEmpleados(unE);
+
+            ObContexto.SaveChanges();
+
+            lblError.ForeColor = Color.Green;
+            lblError.Text = "Se modificó correctamente.";
+
+            LimpiarDatos();
+        }
+        catch (Exception ex)
+        {
+            lblError.ForeColor = Color.Red;
+            lblError.Text = ex.Message;
+        }
+    }
+
+    protected void btnEliminar_Click(object sender, EventArgs e)
+    {
+        Obligatorio2Entities ObContexto = null;
+
+        try
+        {
+            ObContexto = (Obligatorio2Entities)Session["Contexto"];
+            Empleados unE = (Empleados)Session["Empleados"];
+
+            bool find = (from unEmp in ObContexto.Viajes
+                         where unEmp.Empleados.Usuario == unE.Usuario
+                         select unEmp).Any();
+
+            if (find)
+            {
+                lblError.ForeColor = Color.Blue;
+                lblError.Text = "No se puede eliminar, hay viajes asociados.";
+                return;
+            }
+
+            ObContexto.Empleados.Remove(unE);
+            ObContexto.SaveChanges();
+
+            Empleados empLogueado = (Empleados)Session["Usuario"];
+
+            if (unE.Usuario == empLogueado.Usuario )
+            {
+                Session["Usuario"] = null;
+                Response.Redirect("~/default.aspx");
+            }
+
+            lblError.Text = "Eliminación con éxito.";
+
+            LimpiarDatos();
+        }
+        catch (Exception ex)
+        {
+            lblError.ForeColor = Color.Red;
+            lblError.Text = ex.Message;
+        }
+    }
+}
---
+++ TerminalesD2/SitiosTerminales/Default.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ModeloEF;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (!IsPostBack)
+            {
+                GeneroContext();
+                Obligatorio2Entities ObContexto = (Obligatorio2Entities)Session["Contexto"];
+
+                Session["Usuario"] = null;
+                var listadoViajes = ObContexto.Viajes.Where(x => x.FechaPartida > DateTime.Now).ToList();
+
+                var listadoVM = (from unV in listadoViajes
+                                 from unaT in unV.ViajeTerminal
+                                 orderby unV.FechaPartida
+                                 select new
+                                 {
+                                     NumeroDeViaje = unV.CodigoInterno,
+                                     FechaHora = unV.FechaPartida,
+                                     AndenSalida = unV.Anden,
+                                     Destino = unV.ViajeTerminal.OrderByDescending(x => x.NroParada).FirstOrDefault().Terminales.Ciudad
+                                 }).ToList<object>();
+
+                Session["ListaViajes"] = listadoVM;
+                GVListadoPartidas.DataSource = listadoVM;
+                GVListadoPartidas.DataBind();
+            }
+        }
+        catch (Exception ex)
+        {
+            lblError.Text = ex.Message;
+        }
+    }
+
+    private void GeneroContext()
+    {
+        try
+        {
+            Obligatorio2Entities ObContexto = new Obligatorio2Entities();
+
+
+            Session["Contexto"] = ObContexto;
+        }
+        catch (Exception ex)
+        {
+
+            lblError.Text = ex.Message;
+        }
+    }
+
+    protected void btnInicioSession_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Obligatorio2Entities ObContexto = (Obligatorio2Entities)Session["Contexto"];
+
+            Empleados unEmp = ObContexto.Empleados.FirstOrDefault(x => x.Usuario == txtUsu.Text.Trim() && x.Pass == txtPass.Text.Trim());
+
+            if (unEmp != null)
+            {
+                Session["Usuario"] = unEmp;
+                Response.Redirect("~/Menu.aspx");
+            }
+            else
+                lblError.Text = "Credenciales incorrectas";
+        }
+        catch (Exception ex)
+        {
+            lblError.Text = ex.Message;
+        }
+    }
+
+    protected void GVListadoPartidas_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GVListadoPartidas.PageIndex = e.NewPageIndex;
+        GVListadoPartidas.DataSource = Session["ListaViajes"];
+        GVListadoPartidas.DataBind();
+        Page.MaintainScrollPositionOnPostBack = true;
+    }
+
+    
+}
